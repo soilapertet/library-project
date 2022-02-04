057f30b03aa20dd2585b19184dfbb4f110818124
Soila Pertet <nicolepertet@gmail.com>
2022-02-04 18:52:08 +0300

    Create a class function to create new Book objects

---
+++ js/books.js
@@ -0,0 +1,18 @@
+class Book {
+  constructor (
+    name,
+    author,
+    bookStatus,
+    totalPageNumber,
+    currentPageNumber,
+  )
+  {
+    this.name = name;
+    this.author = author;
+    this.bookStatus = bookStatus;
+    this,totalPageNumber = totalPageNumber;
+    this.currentPageNumber = currentPageNumber;
+  }
+}
+
+export default Book;
