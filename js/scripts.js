1c69bbb8dec4684a022c6896464c9ec13422547b
Soila Pertet <nicolepertet@gmail.com>
2022-02-06 16:37:05 +0300

    Create a function to collect the value from the selected radio button

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
 import Book from "./books.js";
 
+// Declare variables
+let statusArray, radioValue,i;
+let bookLibrary = [];
+
+// Collect data from the form
+let inputtedTitle = document.querySelector("#title").value;
+let inputtedAuthor = document.querySelector("#author").value;
+let inputtedCurrentPage = + document.querySelector("#current-page").value;
+let selectedRadioValue = collectRadioValue();
+
 // Trigger the form to scale up and down
 const openForm = document.querySelector("#open-form");
 const closeForm = document.querySelector("#close-form");
 const popUpForm = document.querySelector("#popup-form");
 const overlay = document.querySelector(".overlay");
+const addBookButton = document.querySelector("#add-book");
 
 function openPopupForm() {
   popUpForm.classList.remove("hide");
@@ -21,9 +32,20 @@ function closePopupForm() {
 openForm.addEventListener("click", openPopupForm);
 closeForm.addEventListener("click", closePopupForm);
 
-// Collect data from the form
-const title = document.querySelector("#title").value;
-const author = document.querySelector("#author").value;
-const numberOfPages = + document.querySelector("#total-pages").value;
-const currentPage = + document.querySelector("#current-page").value;
+// Collect the value from the selected radio button 
+function collectRadioValue () {
+
+  // Create a nodeList for input elements with name="book-status"
+  statusArray = document.querySelectorAll("#book-status input");
+
+  // Loop through each item in the nodeList to check which radio button is checked;
+  // Create a variable to store the value of the checked radio button;
+  for(i = 0; i < statusArray.length; i++) {
+    if(statusArray[i].checked) {
+      radioValue = statusArray[i].value;
+    }
+  }
+  return radioValue;
+}
+
 
